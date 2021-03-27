d836e215ae11b934aa08b3473378577abc5ebc1c
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-27 20:53:38 -0300

    conxtet api

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import Modal from "react-modal";
+import { TransactionsContext } from './TransactionsContext'
 
 import { GlobalStyle } from "./styles/global";
 
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -31,6 +32,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
